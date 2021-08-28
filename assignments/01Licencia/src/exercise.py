adf0a5fa988b5a36526b8c11605eaab419e6b6f6
PaulaOSalinas <a01198180@itesm.mx>
2021-08-28 17:03:17 -0500

    0503pm

--- assignments/01Licencia/src/exercise.py
+++ assignments/01Licencia/src/exercise.py
@@ -2,6 +2,15 @@
 def main():
     pass
 
+edad= int(input("Ingresa tu edad: "))
+    if edad >= 18 :
+        identificacion = str(input("¿Tienes identificación oficial? (s/n): "))
+        if identificacion == "s" :
+            print("Trámite de licencia concedido")
+        elif identificacion == "n" :
+            print("No cumples requisitos")
+        else : print("Respuesta incorrecta")
+    else : print("No cumples requisitos")
 
 if __name__ == '__main__':
     main()
