d1b8b9b9966ed20f4bae12cd5949705936b992d3
PaulaOSalinas <a01198180@itesm.mx>
2021-08-29 17:47:51 -0500

    0547pm

--- assignments/05Cuadratica/src/exercise.py
+++ assignments/05Cuadratica/src/exercise.py
@@ -7,5 +7,26 @@ def main():
     c=int(input("Da el valor de c: "))
     pass
 
+    import math
+    a=int(input("Da el valor de a: "))
+    b=int(input("Da el valor de b: "))
+    c=int(input("Da el valor de c: "))
+    discriminante = (b**2) - (4 * a *c)
+    if (a == 0) and (b == 0):
+        print("No tiene solucion")
+    elif (a == 0) and (b != 0):
+        x = (-c) / b
+        print(x)
+    elif (a != 0) and (b != 0):
+        if (discriminante == 0):
+            x1 = (-b)/(2*a)
+            print(x1)
+        elif (discriminante > 0):
+            x2 = (-b - math.sqrt(discriminante)) / (2*a)
+            x3 = (-b + math.sqrt(discriminante))/ (2*a)
+            print(x2)
+            print(x3)
+        else: print("Raices complejas")
+
 if __name__ == '__main__':
     main()
